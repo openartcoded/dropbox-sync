ab0bd5445eebe45379bad92a4f32cd4cdad7bcf7
Aspirio <soultan_98@hotmail.com>
2022-05-03 19:57:56 +0200

    feat: created config class and add automapper, rabbitmq nuggets

---
+++ DropboxSync.BLL/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL
+{
+    public static class Configuration
+    {
+        public static void Config()
+        {
+            // TODO : Mettre en place les injections d'automapper, ef, rabbitmq
+        }
+    }
+}
