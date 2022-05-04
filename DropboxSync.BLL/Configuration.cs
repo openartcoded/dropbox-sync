3e2e5812d247c4202bd428bee117cf795c480f7d
Aspirio <56565073+aspirio187@users.noreply.github.com>
2022-05-04 11:39:56 +0200

    Initialized project (#5)
    * feat: Created solution and 3-tier architecture project
    
    * feat: Added startup.cs, EFCore nuggets and deleted DAL
    
    * feat: created config class and add automapper, rabbitmq nuggets
    
    * feat: added project dependency
    
    * feat: added DependencyInjection nugget
    
    * Changed french comment to english

---
+++ DropboxSync.BLL/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL
+{
+    public static class Configuration
+    {
+        public static void Config()
+        {
+            // Configure injections of Automapper, EFCore, Broker client
+        }
+    }
+}
