b2c79a8a04b4259ffa9cc7cd9f8db2b234e0c5d3
Aspirio <soultan_98@hotmail.com>
2022-05-04 15:39:32 +0200

    create base service interface with basic crud methods

---
+++ DropboxSync.BLL/IServices/IServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL.IServices
+{
+    public interface IServiceBase<TEntity, TKey>
+        where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(TKey id);
+        TEntity Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        bool SaveChanges();
+    }
+}
