7cccaa6ace20151f1438af255bf7f13b6eb4e287
Aspirio <soultan_98@hotmail.com>
2022-05-04 13:20:44 +0200

    feat: created base dbcontext

---
+++ DropboxSync.BLL/DropboxSyncContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL
+{
+    public class DropboxSyncContext : DbContext
+    {
+        public DropboxSyncContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
