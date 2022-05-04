43309b3ac654f7ee40806e0ec2f4a7fe2c821bfd
Aspirio <soultan_98@hotmail.com>
2022-05-04 13:31:12 +0200

    Created Expense Enttiy with properties

---
+++ DropboxSync.BLL/Entities/ExpenseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL.Entities
+{
+    public class ExpenseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Label { get; set; }
+        public string Version { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+        public decimal Vat { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Relations
+        public ICollection<UploadEntity>? Uploads { get; set; }
+    }
+}
