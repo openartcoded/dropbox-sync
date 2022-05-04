1ca65cc6beac3a3d7cfcc6e1c868632f197aba62
Aspirio <soultan_98@hotmail.com>
2022-05-04 16:01:23 +0200

    changed the <see/> tag in comments to <typeparamref/> for more clarity

--- DropboxSync.BLL/IServices/IServiceBase.cs
+++ DropboxSync.BLL/IServices/IServiceBase.cs
@@ -10,35 +10,35 @@ namespace DropboxSync.BLL.IServices
         where TEntity : class
     {
         /// <summary>
-        /// Get all elements of type <see cref="TEntity"/> from the context
+        /// Get all elements of type <typeparamref name="TEntity"/> from the context
         /// </summary>
-        /// <returns><see cref="IEnumerable{TEntity}"/></returns>
+        /// <returns><see cref="IEnumerable{T}"/> where T is of type <typeparamref name="TEntity"/></returns>
         IEnumerable<TEntity> GetAll();
 
         /// <summary>
         /// Return an element of the context by its ID
         /// </summary>
-        /// <param name="id">ID of type <see cref="TKey"/></param>
-        /// <returns>An object of type <see cref="TEntity"/> if any exist. Otherwise return null</returns>
+        /// <param name="id">ID of type <typeparamref name="TKey"/></param>
+        /// <returns>An object of type <typeparamref name="TEntity"/> if any exists. Otherwise return null</returns>
         TEntity GetById(TKey id);
 
         /// <summary>
-        /// Attach a new element of type <see cref="TEntity"/> to the context
+        /// Attach a new element of type <typeparamref name="TEntity"/> to the context
         /// </summary>
-        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
+        /// <param name="entity">Entity of type <typeparamref name="TEntity"/></param>
         void Create(TEntity entity);
 
         /// <summary>
         /// Add an existing modified element to the context
         /// </summary>
-        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
+        /// <param name="entity">Entity of type <typeparamref name="TEntity"/></param>
         void Update(TEntity entity);
 
         /// <summary>
         /// Set an entity from the context as deleted. After the <see cref="SaveChanges()"/> the given entity in parameter
         /// is deleted from the database
         /// </summary>
-        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
+        /// <param name="entity">Entity of type <typeparamref name="TEntity"/></param>
         void Delete(TEntity entity);
 
         /// <summary>
