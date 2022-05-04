ba68fba8a43b926ef02a6827a6bd099ae6629964
Aspirio <soultan_98@hotmail.com>
2022-05-04 17:04:20 +0200

    added warning disable on DropboxSyncContext

--- DropboxSync.BLL/DropboxSyncContext.cs
+++ DropboxSync.BLL/DropboxSyncContext.cs
@@ -16,7 +16,9 @@ namespace DropboxSync.BLL
         public DbSet<InvoiceEntity> Invoices { get; set; }
         public DbSet<UploadEntity> Uploads { get; set; }
 
+#pragma warning disable CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
         public DropboxSyncContext()
+#pragma warning restore CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
         {
 
         }
