c8036b52f24478773911aee17930413608dd5e3d
Aspirio <soultan_98@hotmail.com>
2022-05-04 15:56:44 +0200

    Created DossierService with its crud operation. Added comments to IServiceBase interface

--- DropboxSync.BLL/IServices/IServiceBase.cs
+++ DropboxSync.BLL/IServices/IServiceBase.cs
@@ -9,11 +9,42 @@ namespace DropboxSync.BLL.IServices
     public interface IServiceBase<TEntity, TKey>
         where TEntity : class
     {
+        /// <summary>
+        /// Get all elements of type <see cref="TEntity"/> from the context
+        /// </summary>
+        /// <returns><see cref="IEnumerable{TEntity}"/></returns>
         IEnumerable<TEntity> GetAll();
+
+        /// <summary>
+        /// Return an element of the context by its ID
+        /// </summary>
+        /// <param name="id">ID of type <see cref="TKey"/></param>
+        /// <returns>An object of type <see cref="TEntity"/> if any exist. Otherwise return null</returns>
         TEntity GetById(TKey id);
-        TEntity Create(TEntity entity);
+
+        /// <summary>
+        /// Attach a new element of type <see cref="TEntity"/> to the context
+        /// </summary>
+        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
+        void Create(TEntity entity);
+
+        /// <summary>
+        /// Add an existing modified element to the context
+        /// </summary>
+        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
         void Update(TEntity entity);
+
+        /// <summary>
+        /// Set an entity from the context as deleted. After the <see cref="SaveChanges()"/> the given entity in parameter
+        /// is deleted from the database
+        /// </summary>
+        /// <param name="entity">Entity of type <see cref="TEntity"/></param>
         void Delete(TEntity entity);
+
+        /// <summary>
+        /// Save all the changes in the context to the database
+        /// </summary>
+        /// <returns>true if more than 0 changes occured. false Otherwise</returns>
         bool SaveChanges();
     }
 }
