5fa316a00dd7470ecf6a5f86cbc48ce2de595d58
Aspirio <soultan_98@hotmail.com>
2022-05-04 15:22:25 +0200

    Created upload entity configuration

---
+++ DropboxSync.BLL/Configurations/UploadConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using DropboxSync.BLL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.BLL.Configurations
+{
+    public class UploadConfiguration : IEntityTypeConfiguration<UploadEntity>
+    {
+        public void Configure(EntityTypeBuilder<UploadEntity> builder)
+        {
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.DropboxPath)
+                .IsRequired();
+
+            builder.Property(d => d.BackUpPath)
+                .IsRequired();
+        }
+    }
+}
