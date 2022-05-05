f97b6fcb58e9f108cdadb8edb2e87faa1f6a3360
Aspirio <soultan_98@hotmail.com>
2022-05-05 09:40:28 +0200

    fixed CS8618 warning "DbSet<> can be null value"

--- DropboxSync.BLL/DropboxSyncContext.cs
+++ DropboxSync.BLL/DropboxSyncContext.cs
@@ -11,14 +11,12 @@ namespace DropboxSync.BLL
 {
     public class DropboxSyncContext : DbContext
     {
-        public DbSet<DossierEntity> Dossiers { get; set; }
-        public DbSet<ExpenseEntity> Expenses { get; set; }
-        public DbSet<InvoiceEntity> Invoices { get; set; }
-        public DbSet<UploadEntity> Uploads { get; set; }
+        public DbSet<DossierEntity> Dossiers => Set<DossierEntity>();
+        public DbSet<ExpenseEntity> Expenses => Set<ExpenseEntity>();
+        public DbSet<InvoiceEntity> Invoices => Set<InvoiceEntity>();
+        public DbSet<UploadEntity> Uploads => Set<UploadEntity>();
 
-#pragma warning disable CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
         public DropboxSyncContext()
-#pragma warning restore CS8618 // Un champ non-nullable doit contenir une valeur non-null lors de la fermeture du constructeur. Envisagez de déclarer le champ comme nullable.
         {
 
         }
