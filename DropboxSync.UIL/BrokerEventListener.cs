dc3a752e9a269960e536f39c6ca496141903fa13
Soultan Hatsijev <soultan_98@hotmail.com>
2022-05-06 12:05:24 +0200

    Configured RabbitMQ Client channel listening
    Configured Startup to create new BrokerEventListener with env variable or default values. The listener is then created in the StartUp and started after credentials are correctly retrieved

---
+++ DropboxSync.UIL/BrokerEventListener.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.UIL
+{
+    public class BrokerEventListener
+    {
+        private readonly IConnection _connection;
+        private readonly string _queue;
+
+        public BrokerEventListener(string username, string password, string host, int port, string queue)
+        {
+            if (string.IsNullOrEmpty(username)) throw new ArgumentNullException(nameof(username));
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password));
+            if (string.IsNullOrEmpty(host)) throw new ArgumentNullException(nameof(host));
+            if (string.IsNullOrEmpty(queue)) throw new ArgumentNullException(nameof(queue));
+            if (port <= 0) throw new ArgumentOutOfRangeException(nameof(port));
+
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                UserName = username,
+                Password = password,
+                HostName = host,
+                Port = port
+            };
+
+            _queue = queue;
+
+            _connection = connectionFactory.CreateConnection();
+        }
+
+        public void Start()
+        {
+            var channel = _connection.CreateModel();
+            channel.QueueDeclare(_queue);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += OnQueueMessageReceived;
+
+            channel.BasicConsume(_queue, true, consumer);
+        }
+
+        private void OnQueueMessageReceived(object? sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Message received");
+            var body = e.Body.ToArray();
+            Console.WriteLine($"{Encoding.UTF8.GetString(body)}");
+        }
+    }
+}
--- DropboxSync.UIL/Startup.cs
+++ DropboxSync.UIL/Startup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,19 @@ namespace DropboxSync.UIL
 {
     public static class Startup
     {
+        public static IServiceProvider? ServiceProvider { get; private set; }
 
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            string amqpUsername = Environment.GetEnvironmentVariable("AMQP_USERNAME") ?? "root";
+            string amqpPassword = Environment.GetEnvironmentVariable("AMQP_PASSWORD") ?? "root";
+            string amqpHost = Environment.GetEnvironmentVariable("AMQP_HOST") ?? "localhost";
+            if (!int.TryParse(Environment.GetEnvironmentVariable($"AMQP_PORT"), out int amqpPort)) amqpPort = 61616;
+            string amqpQueue = Environment.GetEnvironmentVariable("AMQP_QUEUE") ?? "backend-event";
+
+            BrokerEventListener brokerEventListener = new BrokerEventListener(amqpUsername, amqpPassword, amqpHost, amqpPort, amqpQueue);
+            if (brokerEventListener is null) throw new ArgumentNullException(nameof(brokerEventListener));
+            brokerEventListener.Start();
+        }
     }
 }
