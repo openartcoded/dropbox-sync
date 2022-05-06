e445a43206d6298e797134a3fa98843e04e5f020
Soultan Hatsijev <soultan_98@hotmail.com>
2022-05-06 16:13:23 +0200

    created model for received expenses

---
+++ DropboxSync.UIL/Models/ExpenseReceivedModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.UIL.Models
+{
+    public class ExpenseReceivedModel
+    {
+        public string ExpenseId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string[] UploadIds { get; set; } = new string[0];
+        public int Timestamp { get; set; } = 0;
+    }
+}
