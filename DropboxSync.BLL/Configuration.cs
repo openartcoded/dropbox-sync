a6a7b12c11237332a076eb5076b17f3e5ac5e716
Soultan Hatsijev <soultan_98@hotmail.com>
2022-05-09 16:12:41 +0200

    created dropbox service and remade Configuration.Config to Configuration.ConfigureBusinessLayer

--- DropboxSync.BLL/Configuration.cs
+++ DropboxSync.BLL/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DropboxSync.BLL.IServices;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,18 @@ namespace DropboxSync.BLL
 {
     public static class Configuration
     {
-        public static void Config()
+        public static IServiceCollection ConfigureBusinessLayer(this IServiceCollection services)
         {
-            // Configure injections of Automapper, EFCore, Broker client
+            services.AddScoped<DropboxSyncContext>();
+
+            services.AddScoped<IExpenseService, IExpenseService>();
+            services.AddScoped<IInvoiceService, IInvoiceService>();
+            services.AddScoped<IDossierService, IDossierService>();
+
+            services.AddScoped<IDropboxService, IDropboxService>();
+            // Configure injections of Automapper
+
+            return services;
         }
     }
 }
