60c6c176b0af6b9598c1db09aeb6c735d1696da4
Soultan Hatsijev <soultan_98@hotmail.com>
2022-05-20 14:16:09 +0200

    created Document models

---
+++ DropboxSync.UIL/Models/Documents/DocumentCreateUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.UIL.Models
+{
+    public class DocumentCreateUpdateModel : DocumentModelBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UploadId { get; set; } = string.Empty;
+        public string? Tag { get; set; }
+    }
+}
---
+++ DropboxSync.UIL/Models/Documents/DocumentRemoveModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxSync.UIL.Models.Documents
+{
+    public class DocumentRemoveModel : DocumentModelBase
+    {
+
+    }
+}
