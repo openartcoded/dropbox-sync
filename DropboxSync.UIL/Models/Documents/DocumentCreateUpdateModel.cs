d1c206875173e75a228003d9b83869dfd6574db0
Soultan <soultan_98@hotmail.com>
2022-06-23 08:57:55 +0200

    add equals and hashcode methods

--- DropboxSync.UIL/Models/Documents/DocumentCreateUpdateModel.cs
+++ DropboxSync.UIL/Models/Documents/DocumentCreateUpdateModel.cs
@@ -12,5 +12,34 @@ namespace DropboxSync.UIL.Models
         public string Description { get; set; } = string.Empty;
         public string UploadId { get; set; } = string.Empty;
         public string? Tag { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DocumentCreateUpdateModel model &&
+                   base.Equals(obj) &&
+                   Timestamp == model.Timestamp &&
+                   Version == model.Version &&
+                   EventName == model.EventName &&
+                   DocumentId == model.DocumentId &&
+                   Title == model.Title &&
+                   Description == model.Description &&
+                   UploadId == model.UploadId &&
+                   Tag == model.Tag;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(base.GetHashCode());
+            hash.Add(Timestamp);
+            hash.Add(Version);
+            hash.Add(EventName);
+            hash.Add(DocumentId);
+            hash.Add(Title);
+            hash.Add(Description);
+            hash.Add(UploadId);
+            hash.Add(Tag);
+            return hash.ToHashCode();
+        }
     }
 }
--- DropboxSync.UIL/Models/Documents/DocumentModelBase.cs
+++ DropboxSync.UIL/Models/Documents/DocumentModelBase.cs
@@ -9,5 +9,20 @@ namespace DropboxSync.UIL.Models
     public class DocumentModelBase : EventModel
     {
         public string DocumentId { get; set; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DocumentModelBase @base &&
+                   base.Equals(obj) &&
+                   Timestamp == @base.Timestamp &&
+                   Version == @base.Version &&
+                   EventName == @base.EventName &&
+                   DocumentId == @base.DocumentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Timestamp, Version, EventName, DocumentId);
+        }
     }
 }
