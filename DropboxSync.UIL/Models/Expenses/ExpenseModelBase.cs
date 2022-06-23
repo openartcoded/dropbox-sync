7b3bfa2e98c5bf23a6fde0087484e2bdc2e42ed1
Soultan <soultan_98@hotmail.com>
2022-06-23 10:10:49 +0200

    added equals and hashcode methods

--- DropboxSync.UIL/Models/Expenses/ExpenseModelBase.cs
+++ DropboxSync.UIL/Models/Expenses/ExpenseModelBase.cs
@@ -9,5 +9,20 @@ namespace DropboxSync.UIL.Models
     public class ExpenseModelBase : EventModel
     {
         public string ExpenseId { get; set; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ExpenseModelBase @base &&
+                   base.Equals(obj) &&
+                   Timestamp == @base.Timestamp &&
+                   Version == @base.Version &&
+                   EventName == @base.EventName &&
+                   ExpenseId == @base.ExpenseId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Timestamp, Version, EventName, ExpenseId);
+        }
     }
 }
--- DropboxSync.UIL/Models/Expenses/ExpensePriceUpdatedModel.cs
+++ DropboxSync.UIL/Models/Expenses/ExpensePriceUpdatedModel.cs
@@ -10,5 +10,22 @@ namespace DropboxSync.UIL.Models
     {
         public decimal PriceHvat { get; set; }
         public decimal Vat { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ExpensePriceUpdatedModel model &&
+                   base.Equals(obj) &&
+                   Timestamp == model.Timestamp &&
+                   Version == model.Version &&
+                   EventName == model.EventName &&
+                   ExpenseId == model.ExpenseId &&
+                   PriceHvat == model.PriceHvat &&
+                   Vat == model.Vat;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Timestamp, Version, EventName, ExpenseId, PriceHvat, Vat);
+        }
     }
 }
